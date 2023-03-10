18565c095792b59a5c1811acc5bcbd7912a8bef6
Bernard <bernie.ibarra99@gmail.com>
2023-03-10 00:07:01 -0500

    fixed error with response and added trending coins divs

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -2,17 +2,18 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const useAxios = (param) => {
-    const [response, setResponse] = useState(null);
+    const [response, setResponse] = useState(null); //useState sets a state and a function to update the state
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState('');
 
-    axios.defaults.baseURL = 'https://api.coingecko.com/api/v3/';
+    axios.defaults.baseURL = 'https://api.coingecko.com/api/v3/'; //url that is loaded on the axios function alongside param
 
     const fetchData = async (param) => {
         try {
             setLoading(true);
-            const result = await axios(param)
-            setResponse(result.data)
+            const result = await axios(param); //param is placed after the baseURL
+            setResponse(result.data); //setResponse updates response to be equal to the data object that is pulled on the axios request
+            // console.log(response);
         }catch(err){
             setError(err);
         }finally{
@@ -20,14 +21,14 @@ const useAxios = (param) => {
         }
     }
 
-    useEffect(() => {
+    useEffect(  () => {       //useEffect runs after page renders, similar to componentDidMount but its used in conjuction with useState
         fetchData(param)
-    }, [])
+    }, []);
 
 
-    return(
-        response, loading, error
-    )
+    return{
+        response, loading, error //returns the values from fetchData
+    }
 }
 
 export default useAxios
--- src/pages/cryptohome.jsx
+++ src/pages/cryptohome.jsx
@@ -1,9 +1,11 @@
 import Trending from '../components/Trending'
+import Markets from '../components/Markets'
 
 const CryptoHome = () => {
     return(
         <div className='wrapper-container'>
             <Trending />
+            <Markets />
         </div>
     )
 }
